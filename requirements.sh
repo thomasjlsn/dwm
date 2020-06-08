98a762a067994922d32d8c088a5f4a350e5883c0
Thomas Ellison <thomasjlsn@gmail.com>
2020-06-08 13:45:30 -0700

    added shell script to install dependencies

---
+++ requirements.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+sudo apt install \
+  fonts-firacode \
+  hsetroot \
+  libx11-dev \
+  libxcb-res0 \
+  libxcb-res0-dev \
+  libxcb1 \
+  libxcb1-dev \
+  libxft-dev \
+  libxinerama-dev \
+  libxll-xcb-dev \
+  libxll-xcb1 \
+  libxrandr-dev
