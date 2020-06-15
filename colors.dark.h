2ff3ea9f756e87a295bd315a7a6cb6efcdf9fc02
Thomas Ellison <thomasjlsn@gmail.com>
2020-06-14 21:46:34 -0700

    added dark colorscheme

---
+++ colors.dark.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#282828";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_gray5[]       = "#000000";
+static const char col_highlight[]   = "#000000";
